56951c9d957c0123f235e86639a91bd5b53222f1
Loïc HAYE <haye.loic@gmail.com>
2018-08-25 15:43:47 +0200

    3d page base structure

---
+++ src/app/three-d-work/three-d-work.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-three-d-work',
+  templateUrl: './three-d-work.component.html',
+  styleUrls: ['./three-d-work.component.css']
+})
+export class ThreeDWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
