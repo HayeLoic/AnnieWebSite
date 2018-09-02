8aa7dfcd89c553967cea89dc640fcbdd25faaa7e
Loïc HAYE <haye.loic@gmail.com>
2018-09-02 18:41:44 +0200

    add full size function
    css class for hovered image (photography and 3d work project)

--- src/app/photography/photography.component.ts
+++ src/app/photography/photography.component.ts
@@ -11,12 +11,26 @@ export class PhotographyComponent implements OnInit {
 
   photographies = null;
   photographiesRepository = '../../assets/photography/';
+  selectedPhotography = null;
+  hoveredPhotography = null;
 
   setImagesLocation(images: Image[], repository: string): Image[] {
     for (let image of images) {
       image.location = repository + image.fileName;
     }
     return images;
+  }
+
+  setSelectedPhotography(photography: Image): void {
+    this.selectedPhotography = photography;
+  }
+
+  setHoveredPhotography(photography: Image): void {
+    this.hoveredPhotography = photography;
+  }
+
+  isHoveredPhotography(photography: Image): boolean {
+    return this.hoveredPhotography == photography;
   }
 
   constructor() { }
